4732c37875f2ab742f7ea31e9baa5749cc790755
mattgefen <matt.gefen@gmail.com>
2021-11-15 14:20:38 -0700

    ex 3 complete

--- exercise-3.py
+++ exercise-3.py
@@ -10,3 +10,21 @@
 #      The dog's age in dog years is xx
 
 # Hint:  Use the int() function to convert the string returned from input() into an integer
+age = ''
+while age.isdigit() == False:
+  age = input('Enter a dog\'s age in human years: ')
+
+age = int(age)
+
+def dog_years(dog_age):
+  acc = 0
+  total = 0
+  for i in range(dog_age):
+    acc += 1
+    if acc in [1,2]:
+      total += 10
+    else:
+      total += 7
+  return total
+
+print(f'The dog\'s age in dog years is {dog_years(age)}')
