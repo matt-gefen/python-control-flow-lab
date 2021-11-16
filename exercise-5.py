bda098a78e38af2bcacf2f3d7bd5ba4bc03df228
mattgefen <matt.gefen@gmail.com>
2021-11-15 19:33:39 -0700

    exercise 5 done

--- exercise-5.py
+++ exercise-5.py
@@ -12,3 +12,13 @@
 #      etc.
 
 # Hint: The next number is found by adding the two numbers before it
+
+def fibonacci():
+  numbers = [0, 1]
+  for i in range(2,50):
+    new_num = numbers[i-2] + numbers[i - 1]
+    numbers.append(new_num)
+  for i in range(50):
+    print(f'term: {i + 1} / number: {numbers[i]}')
+  
+fibonacci()
